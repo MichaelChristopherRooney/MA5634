53454108a1820a214b3bba4f935efebe5a8ddb13
Michael Rooney <m.rooney93@gmail.com>
2017-12-04 16:16:41 +0000

    Estimator variance

--- Assignment 2/Task 3/bernoulli_importance.c
+++ Assignment 2/Task 3/bernoulli_importance.c
@@ -30,6 +30,19 @@ void init_ranlux(){
 	rlxd_init(1, tv.tv_usec); 
 }
 
+double get_std_dev(double avg, double *vals, int n){
+	double res = 0.0;
+	int i;
+	for(i = 0; i < n; i++){
+		double temp = vals[i] - avg;
+		temp = pow(temp, 2);
+		res += temp;
+	}
+	res = res / (n);
+	res = sqrt(res);
+	return res;
+}
+
 #define NUM_RUNS 10000
 
 // Note: a total of NUM_RUNS simulations will be performed using p, n and a
@@ -42,6 +55,8 @@ void run_simulation(double p, int n, int a){
 	printf("Found p' = %f\n", p_prime);
 	printf("Found M(t*) = %f^%d\n", (e_t_star * p) + (1-p), n);
 	double avg_sum = 0;
+	double *est = malloc(sizeof(double) * NUM_RUNS);
+	double avg_est = 0.0;
 	int k;
 	for(k = 0; k < NUM_RUNS; k++){
 		int sum = 0;
@@ -50,10 +65,23 @@ void run_simulation(double p, int n, int a){
 			int brv = generate_bernoulli_rv(p_prime);
 			sum += brv;
 		}
+		if(sum >= a){ // calculate the value of the estimator for this iteration and store it
+			est[k] = pow((1.0/e_t_star), sum) * pow(((e_t_star * p) + (1-p)), n);
+			//printf("Estimator: %f\n", is[k]);
+		} else {
+			est[k] = 0.0;
+		}
+		avg_est += est[k];
 		avg_sum += sum;
 	}
 	avg_sum = avg_sum / NUM_RUNS;
+	avg_est = avg_est / NUM_RUNS;
+	double est_std_dev = get_std_dev(avg_est, est, n);
+	double est_var = pow(est_std_dev, 2);
 	printf("Average sum: %f\n", avg_sum);
+	printf("Average estimator: %.17g\n", avg_est);
+	printf("Estimator standard deviation: %.17g\n", est_std_dev);
+	printf("Estimator variance: %.17g\n", est_var);
 	printf("==================================================\n");
 }
 

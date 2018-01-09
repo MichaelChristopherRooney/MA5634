baf78808a7b11f7c3258514730ea6ca9d142412a
m <m.rooney93@gmail.com>
2018-01-09 09:30:38 -0800

    Cleanup, add more features

--- Assignment 3/metropolis.c
+++ Assignment 3/metropolis.c
@@ -15,9 +15,12 @@ static double g(double x){
 	return temp;
 }
 
-// Simple f(x) for now
-static double f(double x){
+static double cos_x(double x){
 	return cos(x);
+}
+
+static double x_squared(double x){
+	return x*x;
 }
 
 // Generates a RV between in range [0, 1] using RANLUX
@@ -44,36 +47,62 @@ static int accept_or_reject(double x, double x_proposal){
 
 // TODO: accept a function for f(x) and g(x)
 // TODO: seems to be working "ok" with small delta, but I need to verify it
-static double run_metropolis(double x_start, double delta, int num_iter){
+static double *run_metropolis(double x_start, double delta, int num_iter){
 	int num_accepted = 0;
 	int num_rejected = 0;
 	double x = x_start;
 	double x_proposal;
-	double accept_prob;
-	double f_est = 0.0;
-	int therm_limit = 500;
-	int accepted_after_therm = 0;
-	for(int i = 0; i < num_iter; i++){
+	double *results = calloc(num_iter, sizeof(double));
+	results[0] = x_start;
+	for(int i = 1; i < num_iter; i++){ // note: starts at 1
 		x_proposal = generate_x_proposal(x, delta);
 		int accept = accept_or_reject(x, x_proposal);
 		if(accept == 1){
 			num_accepted++;
 			x = x_proposal;
-			// is this the right way to sample ?
-			if(i > therm_limit){
-				accepted_after_therm++;
-				double y = f(x);
-				f_est = f_est + y;
-			}
 		} else {
 			num_rejected++;
 		}
+		results[i] = x;
 	}
-	f_est = f_est / accepted_after_therm;
 	printf("Total accepted: %d\n", num_accepted);
 	printf("Total rejected: %d\n", num_rejected);
-	printf("f(x) est: %f\n", f_est);
-	return 0.0;
+	printf("Acceptance rate: %f%%\n", ((float) num_accepted / (float) num_iter) * 100.0f);
+	return results;
+}
+
+static void save_results(double *results, int size, char *filename){
+	FILE *fp = fopen(filename, "w+");
+	if(fp == NULL){
+		printf("Error: unable to write to file %s\n", filename);
+		return;
+	}
+	int i;
+	for(i = 0; i < size; i++){
+		fprintf(fp, "%f\n", results[i]);
+	}
+	fclose(fp);
+	printf("Markov chain saved to %s\n", filename);
+}
+
+static double estimate_integral(double (*f)(double), char *f_desc, double x_start, double delta, int num_iter, int discard, char *filename){
+	printf("=========================================\n");
+	printf("Statistics for f(x) = %s\n", f_desc);
+	printf("Starting x = %f\n", x_start);
+	printf("Delta = %f\n", delta);
+	printf("Number of iterations = %d\n", num_iter);
+	printf("Discarding first %d results to account for thermalisation\n", discard);
+	double *results = run_metropolis(x_start, delta, num_iter);
+	double estimate = 0.0;
+	int i;
+	for(i = discard; i < num_iter; i++){
+		estimate += f(results[i]);
+	}
+	estimate = estimate / num_iter;
+	save_results(results, num_iter, filename);
+	printf("Estimate is: %f\n", estimate);
+	printf("=========================================\n");
+	return estimate;
 }
 
 void init_ranlux(){
@@ -84,7 +113,8 @@ void init_ranlux(){
 
 int main(void){
 	init_ranlux();
-	run_metropolis(0, 1.5, 1000000);
+	estimate_integral(&cos_x, "cos(x)", 0, 1.5, 1000000, 1000, "cos(x)-0-start-1.5-delta.txt");
+	estimate_integral(&x_squared, "x^2", 0, 1.5, 1000000, 1000, "x*x-0-start-1.5-delta.txt");
 	return 0;
 }
 

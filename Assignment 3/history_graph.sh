aa888f55b3f1688859ca9c4c942e450bd79bf293
m <m.rooney93@gmail.com>
2018-01-16 09:29:02 -0800

    Clean up graphs

--- Assignment 3/history_graph.sh
+++ Assignment 3/history_graph.sh
@@ -5,8 +5,9 @@ set terminal postscript eps enhanced color font 'Helvetica,10'
 #####################
 set output 'graphs/cosx-history.eps'
 set xrange [0:1000]
-set yrange [-10:10]
+set yrange [-3:10]
 set xlabel 'Time'
+set ylabel 'x'
 plot 'data/cosx-1.txt' using 1:2 with lines lt rgb 'black' title 'Delta = 1.5',\
 'data/cosx-2.txt' using 1:2 with lines lt rgb 'red' title 'Delta = 15',\
 'data/cosx-3.txt' using 1:2 with lines lt rgb 'green' title 'Delta = 150'
@@ -15,7 +16,7 @@ plot 'data/cosx-1.txt' using 1:2 with lines lt rgb 'black' title 'Delta = 1.5',\
 #####################
 set output 'graphs/xsqr-history.eps'
 set xrange [0:1000]
-set yrange [-10:10]
+set yrange [-3:10]
 set xlabel 'Time'
 plot 'data/xsqr-1.txt' using 1:2 with lines lt rgb 'black' title 'Delta = 1.5',\
 'data/xsqr-2.txt' using 1:2 with lines lt rgb 'red' title 'Delta = 15',\
